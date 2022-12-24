0b0821e16e3b9493b280a78ca9788fc44e01d61f
Phil Stainer <philstainer.dev@gmail.com>
2022-12-24 15:13:11 +0000

    feat: add progress indicator when removing worktrees

--- src/commands/remove.ts
+++ src/commands/remove.ts
@@ -1,4 +1,6 @@
+import type { IWorktree } from '#/@types/worktree';
 import { removeBranch, throwIfNotInWorktreeRepository } from '#/helpers/git';
+import { ProgressLocation, window } from 'vscode';
 import settings from '../config/settings';
 import { raiseIssue, showUserMessage } from '../helpers/vscode';
 import { getWorktrees } from '../helpers/worktree/getWorktrees';
@@ -24,8 +26,38 @@ export const remove = async () => {
     if (!Array.isArray(selectedWorktrees))
       selectedWorktrees = [selectedWorktrees];
 
-    await Promise.all(
-      selectedWorktrees.map((worktree) => removeWorktree(worktree))
+    await window.withProgress(
+      {
+        location: ProgressLocation.Window,
+        title: 'Removing Worktrees',
+        cancellable: false,
+      },
+      async (progress) => {
+        const worktrees = selectedWorktrees as IWorktree[];
+
+        let percentProgress = 0;
+        let incrementBy = 100 / worktrees.length;
+
+        progress.report({ increment: percentProgress });
+
+        return Promise.all(
+          worktrees.map(async (worktree) => {
+            await removeWorktree(worktree);
+
+            percentProgress += incrementBy;
+
+            const currentlyRemoved = (
+              worktrees.length -
+              (100 - percentProgress) / incrementBy
+            ).toFixed();
+
+            progress.report({
+              increment: percentProgress,
+              message: `${currentlyRemoved}/${worktrees.length}`,
+            });
+          })
+        );
+      }
     );
 
     if (settings.shouldRemoveBranch)
